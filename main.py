67592c189a6b1f0b7fd246be0729aa2011e4a1bb
kettycollins <144057221+kettycollins@users.noreply.github.com>
2024-01-24 22:05:51 +0100

    Update main.py

--- main.py
+++ main.py
@@ -9,11 +9,17 @@ if __name__ == '__main__':
 
     time_small_insertion = timeit.timeit(lambda: insertion_sort(data_small[:]), number=10)
     time_small_merge = timeit.timeit(lambda: merge_sort(data_small[:]), number=10)
+    time_small_timsort = timeit.timeit(lambda: sorted(data_small[:]), number=10)
+    time_small_timsort_s = timeit.timeit(lambda: data_small[:].sort(), number=10)
 
     time_medium_insertion = timeit.timeit(lambda: insertion_sort(data_medium[:]), number=10)
     time_medium_merge = timeit.timeit(lambda: merge_sort(data_medium[:]), number=10)
+    time_medium_timsort = timeit.timeit(lambda: sorted(data_medium[:]), number=10)
+    time_medium_timsort_s = timeit.timeit(lambda: data_medium[:].sort(), number=10)
 
     print(f"{'| Algorithm': <20} | {'Time small data': <20} | {'Time medium data': <20}")
     print(f":{'-'*19} | :{'-'*19} | :{'-'*19}")
     print(f"{'| Insertion sort': <20} | {time_small_insertion:<20.5f} | {time_medium_insertion:<20.5f}")
     print(f"{'| Merge sort': <20} | {time_small_merge:<20.5f} | {time_medium_merge:<20.5f}")
+    print(f"{'| Timsort (sorted)': <20} | {time_small_timsort:<20.5f} | {time_medium_timsort:<20.5f}")
+    print(f"{'| Timsort (sort)': <20} | {time_small_timsort_s:<20.5f} | {time_medium_timsort_s:<20.5f}")
